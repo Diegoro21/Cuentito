ce170fb167556d32ab87c35d4f3ee051377d69ad
Diego <diego.rodriguez@facturante.com>
2024-12-03 15:51:11 -0300

    Pantalla de login

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Text, View, StyleSheet, Alert } from 'react-native';
 import Button from './app/components/Button';
+import Login from './app/screens/Login';
 
 const handleButtonPress = () => {
   console.log('¡Botón presionado!');
@@ -9,20 +10,22 @@ const handleButtonPress = () => {
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hola Mundo</Text>
+      {/* <Text style={styles.text}>Hola Mundo</Text>
       <Button onPress={handleButtonPress} title="Presionar aquí" color='unset' 
       colortext='#595D62' underlayColor='unset' hasBorder={true} borderColor='black' />
-      <Button onPress={handleButtonPress} title="Presionar aquí" />
+      <Button onPress={handleButtonPress} title="Presionar aquí" /> */}
+      <Login />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    width: "100%",
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#f5f5f5',
+    backgroundColor: '#F5F5F5',
   },
   text: {
     fontSize: 20,
